a7ee9557aaffba5ee91cc0febd6a541f860ba989
Ricky610329 <80061973+Ricky610329@users.noreply.github.com>
2023-05-14 17:01:57 +0800

    2313

--- chattest.py
+++ chattest.py
@@ -3,11 +3,14 @@ import threading
 import time
 def show(x):
     while True:
-        print(x.read_queue())
+        y = x.read_queue()
+        if len(y) != 0:
+            print(y.decode('utf-8'))
         time.sleep(0.3)
 agent = chat.chat_manager('192.168.0.162')
 
 agent.chat_init()
-threading.Thread(target=show,args=(agent,))
+th = threading.Thread(target=show,args=(agent,))
+th.start()
 while True:
     agent.send(input(">>>"))
