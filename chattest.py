95ef7b832c5ba3e0bcc7a89232c9bb0ccc961802
Ricky610329 <80061973+Ricky610329@users.noreply.github.com>
2023-05-14 16:56:45 +0800

    efwqd

--- chattest.py
+++ chattest.py
@@ -1,3 +1,13 @@
 from datastream import chat
-
+import threading
+import time
+def show(x):
+    while True:
+        print(x.read_queue())
+        time.sleep(0.3)
 agent = chat.chat_manager('192.168.0.162')
+
+agent.chat_init()
+threading.Thread(target=show,args=(agent,))
+while True:
+    agent.send(input(">>>"))
